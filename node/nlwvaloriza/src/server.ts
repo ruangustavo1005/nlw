62ab26f5ad42d143740dfd24fa8ce6b5e8c697d5
Ruan Gustavo Moretti <ruan.gustavo.moretti10@gmail.com>
2021-09-23 17:38:21 -0300

    finalização

--- node/nlwvaloriza/src/server.ts
+++ node/nlwvaloriza/src/server.ts
@@ -3,10 +3,13 @@ import "express-async-errors";
 import "./database";
 import express, { Request, Response, NextFunction } from "express";
 import { router } from "./routers";
+// import { cors } from "@types/cors";
 
 const app = express();
 
 app.use(express.json());
+
+// app.use(cors());
 
 app.use(router);
 
