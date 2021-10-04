039640dcb6a6052d2f49cc47b09649d9e5a63fc7
Ruan Gustavo Moretti <ruan.gustavo.moretti10@gmail.com>
2021-10-04 16:34:34 -0300

    primeiro dia elixir

---
+++ elixir/wabanex/lib/wabanex/imc.ex
@@ -0,0 +1,31 @@
+defmodule Wabanex.IMC do
+  def calculate(%{"filename" => filename}) do
+    filename
+    |> File.read()
+    |> handle_file()
+  end
+
+  defp handle_file({:ok, content}) do
+    data = content
+    |> String.split("\r\n")
+    |> Enum.map(fn item -> parse_line(item) end)
+    |> Enum.into(%{})
+
+    {:ok, data}
+  end
+
+  defp handle_file({:error, _reason}) do
+    {:error, "Error while opening the file"}
+  end
+
+  defp parse_line(item) do
+    item
+    |> String.split(",")
+    |> List.update_at(1, &String.to_float/1)
+    |> List.update_at(2, &String.to_float/1)
+    |> calc_imc()
+  end
+
+  defp calc_imc([name, height, weight]), do: {name, weight / (height * height)}
+
+end
